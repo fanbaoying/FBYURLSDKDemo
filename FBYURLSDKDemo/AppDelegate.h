3511ead32bf491152a647c9e5861e03b54972623
BaoYing-Fan <1479162730@qq.com>
2018-02-01 10:23:24 +0800

    Initial Commit

---
+++ FBYURLSDKDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FBYURLSDKDemo
+//
+//  Created by 范保莹 on 2018/2/1.
+//  Copyright © 2018年 FBYURLSDKDemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FBYURLSDKDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FBYURLSDKDemo
+//
+//  Created by 范保莹 on 2018/2/1.
+//  Copyright © 2018年 FBYURLSDKDemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
